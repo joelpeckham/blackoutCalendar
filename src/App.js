5d3b2609bf6057fa5fa71e6404a09268e484780a
Joel Peckham <joelskyler@gmail.com>
2022-05-19 18:11:08 -0600

    cal needs styles

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <Calendar />
+        <Calendar year={2022} month = {9}/>
       </header>
     </div>
   );
--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,5 +1,36 @@
 import './Calendar.css';
-function Calendar({month, year}){
-    return <h1>Cal</h1>
+
+function Calendar({year, month}){
+    const englishMonthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    const firstDayOfMonth = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month + 1, 0).getDate();
+    let bodyGrid = [
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>S</p>,
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>M</p>, 
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>T</p>,
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>W</p>,
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>T</p>,
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>F</p>,
+                    <p className='__Calendar_Grid_Item __Calendar_Grid_Col_Label'>S</p>,
+                ];
+    let startDate = new Date(year, month, 1 - firstDayOfMonth);
+    let currentDate = new Date(startDate);
+    for (let i = 0; i < 6*7; i++) {
+        let day = currentDate.getDate();
+        currentDate.setDate(day + 1);
+        let active = (i < firstDayOfMonth || i >= firstDayOfMonth + daysInMonth) ? '__Calendar_Grid_Inactive' : '__Calendar_Grid_Active'
+        let className = '__Calendar_Grid_Item __Calendar_Grid_Num ' + active;
+        bodyGrid.push(<p className={className}>{day}</p>);
+    } 
+    return(
+        <div className="__Calendar">
+            <header className="__Calendar_Header">
+                <h1>{englishMonthNames[month]}</h1>
+            </header>
+            <div className='__Calendar_Body'>
+                {bodyGrid} 
+            </div>
+        </div>
+    )
 }
 export default Calendar;
